da1aff96ecdbd682d4a091ee8cc16f16d61c1e1c
delashoo <delashoo.uk@gmail.com>
2023-04-07 10:39:40 +0300

    Updates
    Updates005

--- drafts/ipfs.sol
+++ drafts/ipfs.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "ipfs-http-client/ src/index.sol";
 
 contract VCS {
-  IPFSHTTPClient ipfs = IPFSHTTPClient("localhost", 5001);
+  IPFSHTTPClient ipfs = IPFSHTTPClient("localhost", 5001); //IPFSHTTPClient library, used to add file to IPFS when commit is created
 
   function commit(string memory branchName, string memory message, string memory ipfsHash) public {
     Version[] memory versions = new Version[](1);
@@ -17,21 +17,8 @@ contract VCS {
   }
 
   function getVersionIPFSHash(string memory branchName, uint256 commitIndex, uint256 versionIndex) public view returns (string memory) {
-    return branches[branchName].commits[commitIndex].versions[versionIndex].ipfsHash;
+    return branches[branchName].commits[commitIndex].versions[versionIndex].ipfsHash; //returns IPFS hash of the specific version
   }
 }
 
 //his code uses the IPFSHTTPClient library to add a file to IPFS when a new commit is created. I
-//t also defines a getVersionIPFSHash function, which returns the IPFS hash of a specific version.
-
-//SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-contract StoreIpfs {
-    function storeFile(bytes memory fileContent) public returns (bytes32) {
-        bytes memory encoded = abi.encodePacked(fileContent);
-        (bool success, bytes memory result) = ipfs.add(encoded);
-        require(success, "Failed to store file on IPFS");
-        return bytes32(result);
-    }
-}
